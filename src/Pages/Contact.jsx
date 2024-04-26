06d7b192cf3ac11c859759f752fc21c8fc4c3fc7
Idba1 <moniraislamself@gmail.com>
2024-04-26 21:14:44 +0600

    about us and contact structure

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,7 +2,64 @@
 const Contact = () => {
     return (
         <div>
-            <h1>Contact</h1>
+
+            <div className="relative w-full flex gap-4 py-6 overflow-x-auto">
+                <img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?1" alt="Image 1" />
+                <img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?2" alt="Image 2" />
+                <img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?3" alt="Image 3" />
+                <img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?4" alt="Image 4" />
+                <img className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?5" alt="Image 5" />
+            </div>
+
+            <div className="text-center mb-10 mt-8 lg:mt-12 p-5">
+                <h1 className="text-2xl  text-sky-950 font-bold mt-2">Touch With Euro Journey</h1>
+                <p className="text-black content-center ">Elevate your communication experience with our exclusive contact page, <br /> designed for seamless interaction with our discerning clientele. Connect effortlessly for <br /> inquiries, assistance, or esteemed collaboration opportunities in the realm of luxury real estate.</p>
+            </div>
+
+            <section className="py-6 dark:bg-gray-100 dark:text-gray-900">
+                <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
+                    <div className="py-6 md:py-0 md:px-6">
+                        <h1 className="text-4xl font-bold">Get in touch</h1>
+                        <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
+                        <div className="space-y-4">
+                            <p className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                    <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Fake address, 9999 City</span>
+                            </p>
+                            <p className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+                                </svg>
+                                <span>123456789</span>
+                            </p>
+                            <p className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                                </svg>
+                                <span>contact@business.com</span>
+                            </p>
+                        </div>
+                    </div>
+                    <form noValidate="" className="flex flex-col py-6 space-y-6 md:py-0 md:px-6">
+                        <label className="block">
+                            <span className="mb-1">Full name</span>
+                            <input type="text" placeholder="Leroy Jenkins" className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:dark:ring-violet-600 dark:bg-gray-100" />
+                        </label>
+                        <label className="block">
+                            <span className="mb-1">Email address</span>
+                            <input type="email" placeholder="leroy@jenkins.com" className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:dark:ring-violet-600 dark:bg-gray-100" />
+                        </label>
+                        <label className="block">
+                            <span className="mb-1">Message</span>
+                            <textarea rows="3" className="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:dark:ring-violet-600 dark:bg-gray-100"></textarea>
+                        </label>
+                        <button type="button" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 dark:bg-violet-600 dark:text-gray-50 focus:dark:ring-violet-600 hover:dark:ring-violet-600">Submit</button>
+                    </form>
+                </div>
+            </section>
         </div>
     );
 };
