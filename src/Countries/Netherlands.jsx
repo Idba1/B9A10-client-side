c64bb26709e16baf3d73ed4c36d3b4b50c32c948
Idba1 <moniraislamself@gmail.com>
2024-04-26 23:45:07 +0600

    photo gallary for netherland

--- src/Countries/Netherlands.jsx
+++ src/Countries/Netherlands.jsx
@@ -3,7 +3,23 @@
 const Netherlands = () => {
     return (
         <div>
-            <h1>Netherlands</h1>
+            <section className="py-6 dark:bg-gray-100">
+                <div className="container flex flex-col justify-center p-4 mx-auto">
+                    <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+                        <img className="object-cover w-full dark:bg-gray-500 aspect-square" src="/public/flower9.jpg" />
+                        <img className="object-cover w-full dark:bg-gray-500 aspect-square" src="/public/netherland2.jpg" />
+                        <img className="object-cover w-full dark:bg-gray-500 aspect-square" src="/public/netherland3.jpg" />
+                        <img className="object-cover w-full dark:bg-gray-500 aspect-square" src="/public/netherland5.jpg" />
+                    </div>
+                    <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+                        <img className="object-cover w-full dark:bg-gray-500 aspect-square" src="/public/netherland6.jpg" />
+                        <img className="object-cover w-full dark:bg-gray-500 aspect-square" src="/public/netherland7.jpg" />
+                        <img className="object-cover w-full dark:bg-gray-500 aspect-square" src="/public/netherland8.jpg" />
+                        <img className="object-cover w-full dark:bg-gray-500 aspect-square" src="/public/banner6.jpg" />
+                    </div>
+                    
+                </div>
+            </section>
         </div>
     );
 };
