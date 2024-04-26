06f33f43225c9c7aa1ab7f1c50dbffb25be57f03
Idba1 <moniraislamself@gmail.com>
2024-04-26 16:30:13 +0600

    add some components and update main

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Navbar from './Components/Navbar';
+// import Navbar from './Components/Navbar';
 import Countries from './Components/Countries';
 import Spain from './Countries/Spain';
 import France from './Countries/France';
@@ -18,58 +18,68 @@ import Contact from './Pages/Contact';
 import ComingSoon from './Pages/ComingSoon';
 import Queastion from './Pages/Queastion';
 import OurTeam from './Pages/OurTeam';
+import Home from './Components/Home';
+import Root from './Components/Root/Root';
+import NotFound from './Pages/NotFound';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navbar></Navbar>,
-  },
-  {
-    path: "/buy",
-    element: <Countries></Countries>,
-  },
-  {
-    path: "/spain",
-    element: <Spain></Spain>,
-  },
-  {
-    path: "/france",
-    element: <France></France>,
-  },
-  {
-    path: "/italy",
-    element: <Italy></Italy>,
-  },
-  {
-    path: "/netherlands",
-    element: <Netherlands></Netherlands>,
-  },
-  {
-    path: "/england",
-    element: <England></England>,
-  },
-  {
-    path: "/swizerland",
-    element: <Switzerland></Switzerland>,
-  },
-  {
-    path: "/ourteam",
-    element: <OurTeam></OurTeam>,
-  },
-  {
-    path: "/comingsoon",
-    element: <ComingSoon></ComingSoon>,
-  },
-  {
-    path: "/about",
-    element: <AboutUs></AboutUs>,
-  },
-  {
-    path: "/f&Q",
-    element: <Queastion></Queastion>,
-  },
-  {
-    path: "/contact",
-    element: <Contact></Contact>,
+    element: <Root></Root>,
+    errorElement: <NotFound></NotFound>,
+    children: [
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/buy",
+        element: <Countries></Countries>,
+      },
+      {
+        path: "/spain",
+        element: <Spain></Spain>,
+      },
+      {
+        path: "/france",
+        element: <France></France>,
+      },
+      {
+        path: "/italy",
+        element: <Italy></Italy>,
+      },
+      {
+        path: "/netherlands",
+        element: <Netherlands></Netherlands>,
+      },
+      {
+        path: "/england",
+        element: <England></England>,
+      },
+      {
+        path: "/swizerland",
+        element: <Switzerland></Switzerland>,
+      },
+      {
+        path: "/ourteam",
+        element: <OurTeam></OurTeam>,
+      },
+      {
+        path: "/comingsoon",
+        element: <ComingSoon></ComingSoon>,
+      },
+      {
+        path: "/aboutus",
+        element: <AboutUs></AboutUs>,
+      },
+      {
+        path: "/f&Q",
+        element: <Queastion></Queastion>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
