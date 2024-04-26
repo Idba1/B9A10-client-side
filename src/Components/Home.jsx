3e35ac3cd351ee5b928d732adb7b5d2d61346b1c
Idba1 <moniraislamself@gmail.com>
2024-04-26 18:19:08 +0600

    update tittle and logo

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,11 @@
+import Slider from "./Slider";
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <Slider></Slider>
+            <img src="../../public/Country/banner/banner4.jpg" alt="" />
+            <h1>hello</h1>
         </div>
     );
 };
