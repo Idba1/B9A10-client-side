b065a16a2a32568a3eb4b6535d71e4e5d77cd05c
Idba1 <moniraislamself@gmail.com>
2024-04-26 21:22:59 +0600

    page structure

--- src/Nav/Page.jsx
+++ src/Nav/Page.jsx
@@ -1,8 +1,21 @@
+import Slider from "../Components/Slider";
+import AboutUs from "../Pages/AboutUs";
+import ComingSoon from "../Pages/ComingSoon";
+import Contact from "../Pages/Contact";
+import OurTeam from "../Pages/OurTeam";
+import Queastion from "../Pages/Queastion";
+import Ticket from "../Pages/Ticket";
 
 const Page = () => {
     return (
         <div>
-            <h1>page</h1>
+            <Slider></Slider>
+            <Queastion></Queastion>
+            <ComingSoon></ComingSoon>            
+            <Contact></Contact>
+            <Ticket></Ticket>
+            <OurTeam></OurTeam>
+            <AboutUs></AboutUs>
         </div>
     );
 };
