da7924f03e05715f1ac8e67031081dee10dda876
Idba1 <moniraislamself@gmail.com>
2024-04-26 21:28:17 +0600

    my add section structure

--- src/Nav/MyAdd.jsx
+++ src/Nav/MyAdd.jsx
@@ -2,7 +2,52 @@
 const MyAdd = () => {
     return (
         <div>
-            <h1>My Add</h1>
+
+
+            <div className="carousel rounded-box">
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+                </div>
+            </div>
+
+            <div className="text-center mb-10 mt-8 lg:mt-12 p-5">
+                <h1 className="text-2xl  text-sky-950 font-bold mt-2">Touch With Euro Journey</h1>
+                <p className="text-black content-center ">Elevate your communication experience with our exclusive contact page, <br /> designed for seamless interaction with our discerning clientele. Connect effortlessly for <br /> inquiries, assistance, or esteemed collaboration opportunities in the realm of luxury real estate.</p>
+            </div>
+
+
+            <div className="card card-side bg-base-100 shadow-xl">
+                <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
     );
 };
