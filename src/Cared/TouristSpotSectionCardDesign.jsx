a4828eb5101052f89cc8bc93414ca188984a9257
Idba1 <moniraislamself@gmail.com>
2024-04-28 21:53:02 +0600

    tourist spot section card design structure

--- src/Cared/TouristSpotSectionCardDesign.jsx
+++ src/Cared/TouristSpotSectionCardDesign.jsx
@@ -1,10 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const TouristSpotSectionCardDesign = () => {
-    const loadedspots = useLoaderData();
+const TouristSpotSectionCardDesign = ({spot}) => {
+    const { _id, image, tourists_spot_name, country_Name, location, short_description, average_cost, seasonality, travel_time, totaVisitorsPerYear, email, name } = spot;
     return (
         <div>
-            <h1>spot: {loadedspots.length}</h1>
+            <div className="card card-side bg-base-100 shadow-xl">
+                <figure><img src= { image} alt="Movie" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">  {tourists_spot_name} </h2>
+                    <p> {short_description} </p>
+                    <div className="card-actions justify-end">
+                        <Link to={`/viewdetails`} className="btn btn-primary">View Details</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Nav/TouristSpotSection.jsx
+++ src/Nav/TouristSpotSection.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import TouristSpotSectionCardDesign from "../Cared/TouristSpotSectionCardDesign";
 
 const TouristSpotSection = () => {
     const touristSpot = useLoaderData();
+     const [spots, setspots] = useState(touristSpot);
     return (
         <div>
             <h1>spot: {touristSpot.length}</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    spots.map(spot => <TouristSpotSectionCardDesign
+                        key={spot._id}
+                        spot={spot}
+                        spots={spots}
+                        setspots={setspots}
+                    ></TouristSpotSectionCardDesign>)
+                }
+            </div>
         </div>
     );
 };
