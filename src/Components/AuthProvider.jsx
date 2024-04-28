6a827bd134036deed52f73f2f7790ab0d3f1b31d
Idba1 <moniraislamself@gmail.com>
2024-04-29 04:37:46 +0600

    user create in firebase with email and pass

--- src/Components/AuthProvider.jsx
+++ src/Components/AuthProvider.jsx
@@ -1,8 +1,27 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import PropTypes from 'prop-types'
+import { createContext } from 'react';
+import auth from '../Firebase/Firebase.init';
+export const AuthContext = createContext(null)
+
+
 const AuthProvider = ({ children }) => {
+
+      // create user
+      const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+
+    const allvalues = { createUser
+
+    };
+
     return (
         <div>
-            { children }
+            <AuthContext.Provider value={allvalues}>
+                {children}
+            </AuthContext.Provider>
         </div>
     );
 };
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,30 +1,69 @@
+import { useContext } from "react";
+import { AuthContext } from "./AuthProvider";
+import { useForm } from "react-hook-form";
 
 
 const Register = () => {
+    const { createUser } = useContext(AuthContext)
+    
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        const { email, password, image, fullName } = data;
+
+
+        createUser(email, password)
+            .then((result) => {
+                console.log(result);
+            });
+    };
+
+    
     return (
         <div className="w-full max-w-md mx-auto my-12 p-8 space-y-3 rounded-xl dark:bg-gray-50 dark:text-gray-800">
         <h1 className="text-2xl font-bold text-center">Register</h1>
-        <form  noValidate="" action="" className="space-y-6">
+        <form onSubmit={handleSubmit(onSubmit)} noValidate="" action="" className="space-y-6">
             <div className="space-y-1 text-sm">
                 <label htmlFor="username" className="block dark:text-gray-600">Name</label>
-                <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-sky-950"
+                <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-sky-950" 
+                {...register("FullName", { required: true })} 
                  />
+                 {errors.FullName && (
+                            <span className="text-red-500">This field is required</span>
+                        )}
                 
             </div>
             <div className="space-y-1 text-sm">
                 <label htmlFor="email" className="block dark:text-gray-600">Email</label>
-                <input type="email" name="email" id="email" placeholder="email" className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-sky-950"
-                
+                <input type="email" name="email" id="email" placeholder="email" className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-sky-950" 
+                {...register("email", { required: true })} 
                 />
+                {errors.email && (
+                            <span className="text-red-500">This field is required</span>
+                        )}
             </div>
             <div className="space-y-1 text-sm">
                 <label htmlFor="photo_url" className="block dark:text-gray-600">Photo Url</label>
-                <input type="text" name="photo_url" id="photo_url" placeholder="photo_url" className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-sky-950" />
+                <input type="text" name="photo_url" id="photo_url" placeholder="photo_url" className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-sky-950" 
+                {...register("photo_url", { required: true })}  
+                />
+                {errors.photo_url && (
+                            <span className="text-red-500">This field is required</span>
+                        )}
             </div>
 
             <div className="space-y-1 text-sm relative">
                 <label htmlFor="password" className="block dark:text-gray-600">Password</label>
-                <input name="password" id="password" placeholder="Password" className="w-full px-4 py-2 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-[#1E0342]"/>
+                <input name="password" id="password" placeholder="Password" className="w-full px-4 py-2 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-[#1E0342]"
+                {...register("password", { required: true })}
+                />
+                {errors.password && (
+                            <span className="text-red-500">This field is required</span>
+                        )}
             </div>
             <button className="block w-full p-3 text-center  dark:text-black font-semibold rounded-md dark:bg-[#9AC8CD]">Register
             </button>
